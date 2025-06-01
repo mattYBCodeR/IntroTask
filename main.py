7d6dd4362fa5485f65348ec11a69843cec1c5df6
mattYBCodeR <166936467+mattYBCodeR@users.noreply.github.com>
2025-06-01 16:36:24 -0400

    added gitignore and env

---
+++ main.py
@@ -0,0 +1,7 @@
+def hello(name):
+    print(f"Hello, {name}!")
+
+
+
+if __name__ =="__main__":
+    hello("World")
--- task.py
+++ task.py
@@ -1,7 +1,13 @@
-def hello(name):
-    print(f"Hello, {name}!")
+import os
+import pandas as pd
+from dotenv import load_dotenv
 
+load_dotenv()
 
+def nasa_firms_api():
+    NASA_KEY = os.getenv('NASA_KEY')
+    data_url = f'https://firms.modaps.eosdis.nasa.gov/api/data_availability/csv/{NASA_KEY}/all'
+    df = pd.read_csv(data_url)
+    print(df.head())
+    return df
 
-if __name__ =="__main__":
-    hello("World")
