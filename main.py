f1018a5480efc841e78ccde746cd11a12561825e
mattYBCodeR <166936467+mattYBCodeR@users.noreply.github.com>
2025-06-09 23:11:37 -0400

    extracted relevant data with SOURCE, AREA COORDIANTES, DAY RANGE, and DATE. returned 2 seperate dfs for MODIS adn VIIRS then put them into their own corresponding JSON files

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ from task import *
 if __name__ =="__main__":
     print(nasa_firms_api())
     print()
-    print(noaa20_data())
+    print(dataSensors_data())
+
+    print()
+    print()
+    print_dfs()
+    df_to_JSON()
